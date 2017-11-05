70da7c2b8900e0bee866018732840729dbd09a2d
GracieChristine <graciechristineli@gmail.com>
2017-11-04 22:10:49 -0600

    Moving calendar, date, dates components into /calendar. Adding event and events components. Display events under dates.

--- App.js
+++ App.js
@@ -1,18 +1,56 @@
 import React from 'react';
 import {StyleSheet, StatusBar, View} from 'react-native';
-import Calendar from './src/Calendar';
+import faker from 'faker';
+import moment from 'moment';
+import Calendar from './src/calendar/Calendar';
+import Events from './src/events/Events';
 import type Moment from 'moment';
+
+export type EventType = {
+  date: Moment,
+  title: string,
+  description: string,
+  image: string,
+};
+
+// Generate fake event data
+const FAKE_EVENTS: Array<EventType> = (() => {
+  const startDay = moment().subtract(5, 'days').startOf('day');
+  return [...new Array(64)].map(_ => ({
+    date: startDay.add(4, 'hours').clone(),
+    title: faker.company.companyName(),
+    description: faker.lorem.sentence(),
+    // use random dimensions to get random urls
+    image: faker.image.nightlife(Math.floor(Math.random() * 200) + 100, Math.floor(Math.random() * 200) + 100),
+  }));
+})();
+
+// Filter events by date
+const filterEvents = (date: Moment): ?Array<EventType> =>
+  FAKE_EVENTS.filter(event => event.date.isSame(date, 'day'));
+
 
 export default class App extends React.Component {
 
-  onSelectDate = (date : Moment) => {
-    alert(date.calendar());
+  state = {
+    events: filterEvents(moment()),
+  };
+
+  onSelectDate = (date: Moment) => {
+    this.setState({ events: filterEvents(date) });
   };
 
   render() {
+
+    const { events } = this.state;
+
     return (<View style={styles.container}>
       <StatusBar hidden={true}/>
-      <Calendar onSelectDate={this.onSelectDate}/>
+      <Calendar
+        showDaysAfterCurrent={45}
+        onSelectDate={this.onSelectDate}
+      />
+      <Events events={events} />
     </View>);
   }
 }
