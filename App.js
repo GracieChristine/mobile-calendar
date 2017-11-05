9a9842f93d5d1ebbf328f2bfa2ad91de3a9ec81d
GracieChristine <graciechristineli@gmail.com>
2017-11-04 21:10:56 -0600

    Creating Calendar component and displaying an array of date.

--- App.js
+++ App.js
@@ -1,23 +1,26 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {StyleSheet, StatusBar, View} from 'react-native';
+import Calendar from './src/Calendar';
+import type Moment from 'moment';
 
 export default class App extends React.Component {
+
+  onSelectDate = (date : Moment) => {
+    alert(date.calendar());
+  };
+
   render() {
-    return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
-      </View>
-    );
+    return (<View style={styles.container}>
+      <StatusBar hidden={true}/>
+      <Calendar onSelectDate={this.onSelectDate}/>
+    </View>);
   }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    backgroundColor: '#3F53B1',
+    paddingTop: 20
+  }
 });
